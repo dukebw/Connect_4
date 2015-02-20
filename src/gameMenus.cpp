d0726285f9feb01f3830e19960650a6ae02d137c
jean-lucas <jeanlpf@hotmail.com>
2015-02-20 02:01:46 -0500

    New boundaries for buttons in handleMainMenuMouseClick(int x, int y)

--- src/gameMenus.cpp
+++ src/gameMenus.cpp
@@ -7,6 +7,18 @@
 
 // NOTE(Zach): Determine next MenuState based on where the user clicked
 MenuState handleMainMenuMouseClick(int x, int y) {
+
+  // NOTE(Jean): New values corresponding to the main screen layout
+  //     please note that a new MenuState has been added : "SETUPTWOPLATER"
+
+  //if (x >= 420 && y >= 405 && x <= 560 && y <= 435) return ONEPLAYER; 
+  //if (x >= 580 && y>= 405 && x <= 710 && y <= 435) return TWOPLAYER; 
+  //if (x >= 420 && y>= 565 && x <= 560 && y <= 595) return SETUPONEPLAYER; 
+  //if (x >= 580 && y>= 565 && x <= 710 && y <= 595) return SETUPTWOPLAYER; 
+  //if (x >= 975 && y>= 745 && x <= 1070 && y <= 770) return CREDITS; 
+  //if (x >= 500 && y>=650 && x <= 630 && y <= 710) return QUIT;
+
+  // NOTE(Jean): These are the old values created by someone else
   //if (x >= 102 && y>= 145 && x <= 398 && y <= 211) return ONEPLAYER;
   //if (x >= 96 && y>= 221 && x <= 406 && y <= 287) return TWOPLAYER;
   //if (x >= 96 && y>= 221 && x <= 406 && y <= 287) return SETUP;
