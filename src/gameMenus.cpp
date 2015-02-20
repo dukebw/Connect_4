5c74b86b54d04c917040a311c8fd14f75cbd3064
jean-lucas <jeanlpf@hotmail.com>
2015-02-20 14:02:24 -0500

    Fixed boundary points for home screen, setup and exit buttons are working now

--- src/gameMenus.cpp
+++ src/gameMenus.cpp
@@ -6,34 +6,15 @@
 #include "graphics.h"
 
 // NOTE(Zach): Determine next MenuState based on where the user clicked
+// NOTE(Jean): Values fixed for the new modified and re-scaled image
 MenuState handleMainMenuMouseClick(int x, int y) {
 
-	// TODO(Zach): Make it so there is just one setup state
-	// When setup is complete, one player or two player will be selected
-	// from there
+  //if (x >= 405 && y >= 455 && x <= 511 && y <= 490) return ONEPLAYER; 
+  //if (x >= 530 && y>= 455 && x <= 642 && y <= 490) return TWOPLAYER; 
+  if (x >= 430 && y>= 545 && x <= 602 && y <= 610) return SETUP; 
+  if (x >= 452 && y>=700 && x <= 575 && y <= 760) return QUIT;
+  //if (x >= 890 && y>= 840 && x <= 972 && y <= 868) return CREDITS; 
 
-  // NOTE(Jean): New values corresponding to the main screen layout
-  //     please note that a new MenuState has been added : "SETUPTWOPLATER"
-
-	// TODO(Zach): fix all these layout values so they are correct
-  //if (x >= 420 && y >= 405 && x <= 560 && y <= 435) return ONEPLAYER; 
-  //if (x >= 580 && y>= 405 && x <= 710 && y <= 435) return TWOPLAYER; 
-  //if (x >= 420 && y>= 565 && x <= 560 && y <= 595) return SETUPONEPLAYER; 
-  //if (x >= 580 && y>= 565 && x <= 710 && y <= 595) return SETUPTWOPLAYER; 
-  //if (x >= 975 && y>= 745 && x <= 1070 && y <= 770) return CREDITS; 
-  if (x >= 500 && y>=650 && x <= 630 && y <= 710) return QUIT;
-
-  // NOTE(Jean): These are the old values created by someone else
-  //if (x >= 102 && y>= 145 && x <= 398 && y <= 211) return ONEPLAYER;
-  //if (x >= 96 && y>= 221 && x <= 406 && y <= 287) return TWOPLAYER;
-  //if (x >= 96 && y>= 221 && x <= 406 && y <= 287) return SETUP;
-  //if (x >= 338 && y>= 441 && x <= 493 && y <= 493) return CREDITS;
-  //if (x >= 190 && y>= 297 && x <= 313 && y <= 353) return QUIT;
-
-	// TODO(Zach): something with these layout values isn't right -- fix
-  // NOTE(Zach): TEMPORARY
-  if (x >= 420 && y>= 565 && x <= 560 && y <= 595) return SETUP; 
-  if (x >= 580 && y>= 565 && x <= 710 && y <= 595) return SETUP; 
   return MAINMENU;
 }
 
